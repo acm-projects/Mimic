7e639b9df31395a18de6d8cf95447d5fbcdad6d1
vaishunall <vaishunalla@yahoo.com>
2021-10-17 22:00:36 -0500

    fixed settings screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,20 +1,47 @@
+// import React, { Component } from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import 'react-native-gesture-handler';
+// import { NavigationContainer } from '@react-navigation/native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Setting Screen</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+//This is an example code for Navigator// 
 import React, { Component } from 'react';
-import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
+//import react in our code. 
+import { StyleSheet, View, Text} from 'react-native';
+//import all the components we are going to use.
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Setting Screen</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class SecondPage extends Component {
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text>You are on SecondPage</Text>
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    margin:50,
     alignItems: 'center',
     justifyContent: 'center',
   },
