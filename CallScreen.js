96af710246f9d4e99621959004adf338af8e8239
Hyrum66 <hyrummoses66@gmail.com>
2021-10-19 19:56:59 -0500

    finished with draft call screen, needs fine tuning via screenshot overlay

--- CallScreen.js
+++ CallScreen.js
@@ -19,23 +19,55 @@ const CallScreen = () => {
     <View style={styles.screen}>
       <ImageBackground style={styles.imgBackground}
         source = {require('./assets/images/background.jpg')} // load selected background to this location
-        blurRadius = {15}>
-	        <Text style = {styles.userId}>Insert Name Here</Text>
+        blurRadius = {30}>
+	        <Text style = {styles.userId}>Billie Bob Joe</Text>
+
+          <View style = {styles.staticImageContainer}>
+            <Image
+              style={styles.staticImage}
+              source={require('./assets/images/remind.png')}
+            />
+
+            <Image
+              style={styles.staticImage}
+              source={require('./assets/images/message.png')}
+            />
+          </View>
+
+          <View style = {styles.staticImageTextContainer}>
+            <Text style = {styles.buttonText}>
+              Remind Me
+            </Text>
+
+            <Text style = {styles.buttonText}>
+              Message
+            </Text>
+          </View>
 
           <View style = {styles.buttonContainer}>
-              <TouchableOpacity>
-                <Image
-                  style={styles.button}
-                  source={require('./assets/images/accept.png')}
-                />
-              </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={styles.button}
+                source={require('./assets/images/decline.png')}
+              />
+            </TouchableOpacity>
 
-              <TouchableOpacity>
-                <Image
-                  style={styles.button}
-                  source={require('./assets/images/decline.png')}
-                />
-              </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={styles.button}
+                source={require('./assets/images/accept.png')}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <View style = {styles.buttonTextContainer}>
+            <Text style = {styles.buttonText}>
+              Decline
+            </Text>
+
+            <Text style = {styles.buttonText}>
+              Accept
+            </Text>
           </View>
 			</ImageBackground>
     </View>
@@ -61,15 +93,46 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     justifyContent: "space-between",
-    top: "95%",
+    top: "80%",
     flexDirection: "row",
     width: "100%",
   },
   button: {
     height: width/5.25,
     width: width/5.25,
+    margin: "20%"
+  },
+  buttonTextContainer: {
+    position: 'absolute',
+    justifyContent: "space-between",
+    top: "79%",
+    flexDirection: "row",
+    width: "100%",
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 15,
+    margin: "14%"
+  },
+  staticImageContainer: {
+    position: 'absolute',
+    justifyContent: "space-between",
+    top: "49%",
+    flexDirection: "row",
+    width: "100%",
+  },
+  staticImage: {
+    height: width/10,
+    width: width/10,
     margin: "15%"
-    }
+  },
+  staticImageTextContainer: {
+    position: 'absolute',
+    justifyContent: "space-between",
+    top: "57%",
+    flexDirection: "row",
+    width: "100%",
+  }
 });
 
 export default CallScreen;
